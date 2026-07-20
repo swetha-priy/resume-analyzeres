115c294dc8114f6f078a01929ac7da5090342ce9
Swetha Priya <swethatirumalasetty33@gmail.com>
2026-07-20 10:32:14 +0530

    Fix spaCy model loading

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
